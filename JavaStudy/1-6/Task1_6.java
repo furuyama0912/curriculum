2d75ae79fbb335f238845bd1fdb46582becf3fc1
furuyama0912 <68705777+furuyama0912@users.noreply.github.com>
2020-10-23 16:20:09 +0900

    タスク1－5

--- JavaStudy/1-6/Task1_6.java
+++
@@ -1,9 +0,0 @@
-public class Task1_6 {
-public static void main(String[] args) {
-String[]fluits={"みかん","りんご","ぶどう","メロン"};
-
-for(int i = 0; i < fluits.length; i++) {
-    System.out.println(fluits[ i ]);
-}
-}
-}
